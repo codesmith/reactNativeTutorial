b517a527dc20a5ecad3b364003c824142b36d3d7
yonejima <catchandrelease9@gmail.com>
2023-07-10 23:39:10 +0900

    「なぜイミュータビリティが重要なのか」まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 
-function Square({value}) {
-
-    // function clickHandler() {
-    //     setValue("X");
-    // }
+function Square({value, clickSquareHandler}) {
 
     return (
     <button 
         className="square"
-        // onClick={clickHandler}
+        onClick={clickSquareHandler}
     >
         {value}
     </button>
@@ -19,22 +15,28 @@ function Square({value}) {
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    function clickHandler(i) {
+        const squareList = squares.slice();
+        squareList[i] = "X";
+        setSquares(squareList);
+    }
+
     return (
         <>
             <div className="board-row">
-                <Square value={squares[0]}/>
-                <Square value={squares[1]}/>
-                <Square value={squares[2]}/>
+                <Square value={squares[0]} clickSquareHandler={() => clickHandler(0)}/>
+                <Square value={squares[1]} clickSquareHandler={() => clickHandler(1)}/>
+                <Square value={squares[2]} clickSquareHandler={() => clickHandler(2)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[3]}/>
-                <Square value={squares[4]}/>
-                <Square value={squares[5]}/>
+                <Square value={squares[3]} clickSquareHandler={() => clickHandler(3)}/>
+                <Square value={squares[4]} clickSquareHandler={() => clickHandler(4)}/>
+                <Square value={squares[5]} clickSquareHandler={() => clickHandler(5)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[6]}/>
-                <Square value={squares[7]}/>
-                <Square value={squares[8]}/>
+                <Square value={squares[6]} clickSquareHandler={() => clickHandler(6)}/>
+                <Square value={squares[7]} clickSquareHandler={() => clickHandler(7)}/>
+                <Square value={squares[8]} clickSquareHandler={() => clickHandler(8)}/>
             </div>
         </>
     );
