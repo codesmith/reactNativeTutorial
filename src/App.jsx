cf9402bdc8fa374e7640fb40fb091863ba911983
yonejima <catchandrelease9@gmail.com>
2023-07-10 22:32:55 +0900

    「インタラクティブなコンポーネントの作成」まで2

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function clickHandler() {
+        setValue("X");
+    }
+
+    return (
+    <button 
+        className="square"
+        onClick={clickHandler}
+    >
+        {value}
+    </button>
+    );
+}
+
+export default function Board() {
+  return (
+    <>
+        <div className="board-row">
+            <Square value="1"/>
+            <Square value="2"/>
+            <Square value="3"/>
+        </div>
+        <div className="board-row">
+            <Square value="4"/>
+            <Square value="5"/>
+            <Square value="6"/>
+        </div>
+        <div className="board-row">
+            <Square value="7"/>
+            <Square value="8"/>
+            <Square value="9"/>
+        </div>
+    </>
+    );
+  }
