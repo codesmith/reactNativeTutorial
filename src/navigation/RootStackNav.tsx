116b88dd3ef9c22393ead0a221fc3b770ff24c90
yonejima <catchandrelease9@gmail.com>
2023-07-19 18:13:17 +0900

    「Stackナビゲーション」-「ヘッダータイトルのカスタマイズ」を追加

--- src/navigation/RootStackNav.tsx
+++ src/navigation/RootStackNav.tsx
@@ -1,12 +1,31 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {Welcome, Instructions} from 'screens';
+import {Welcome, Instructions, Login, TodoBoard} from 'screens';
 
 const nav = createStackNavigator();
 export const RootStackNav: React.FC = () => {
   return (
     <nav.Navigator initialRouteName={Welcome.name}>
-      <nav.Screen name="Welcome" component={Welcome} options={{headerShown: false}} />
+      <nav.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{
+          headerTitle: 'Welcome',
+        }} />
+      <nav.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerTitle: 'ログイン',
+        }}
+      />
+      <nav.Screen
+        name="TodoBoard"
+        component={TodoBoard}
+        options={{
+          headerTitle: 'Todo',
+        }}
+      />
       <nav.Screen name="Instructions" component={Instructions} />
     </nav.Navigator>
   );
