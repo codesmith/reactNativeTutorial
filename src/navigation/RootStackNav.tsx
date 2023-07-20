8c0dd1efc40e5b58cdc483ba0f57e582a8b8d48a
yonejima <catchandrelease9@gmail.com>
2023-07-20 13:22:54 +0900

    「状態に応じたナビゲーション」完成

--- src/navigation/RootStackNav.tsx
+++ src/navigation/RootStackNav.tsx
@@ -1,53 +1,20 @@
-import {useNavigation} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useUserContext } from 'contexts/UserContext';
 import React from 'react';
-import {Button} from 'react-native-elements';
-import {Welcome, Instructions, Login, TodoBoard} from 'screens';
 
-const HeaderRight: React.FC = () => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      type="clear"
-      onPress={() => navigation.navigate('Login')}
-      title='ログイン'
-    />
-  );
-};
+import { AuthedStackNav } from './AuthedStackNav';
+import { UnauthedStackNav } from './UnauthedStackNav';
 
-const nav = createStackNavigator();
+const rootNav = createStackNavigator();
 export const RootStackNav: React.FC = () => {
+  const userContext = useUserContext();
   return (
-    <nav.Navigator
-      initialRouteName={Welcome.name}
-      screenOptions={{
-        headerRight: () => <HeaderRight />,
-      }}
-    >
-      <nav.Screen
-        name="Welcome"
-        component={Welcome}
-        options={{
-          headerTitle: 'Welcome',
-        }}
-      />
-      <nav.Screen
-        name="Login"
-        component={Login}
-        options={{
-          headerTitle: 'ログイン',
-          headerRight: undefined,
-        }}
-      />
-      <nav.Screen
-        name="TodoBoard"
-        component={TodoBoard}
-        options={{
-          headerTitle: 'Todo',
-          headerRight: undefined,
-        }}
-      />
-      <nav.Screen name="Instructions" component={Instructions} />
-    </nav.Navigator>
+    <rootNav.Navigator screenOptions={{ headerShown: false }}>
+      {userContext.isLoggedIn ? (
+        <rootNav.Screen name="Authed" component={AuthedStackNav} />
+      ) : (
+        <rootNav.Screen name="Unauthed" component={UnauthedStackNav} />
+      )}
+    </rootNav.Navigator>
   );
 };
