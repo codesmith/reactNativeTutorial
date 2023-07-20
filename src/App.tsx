0848e46edd0638a8624cdc1ba7b79095da31efde
yonejima <catchandrelease9@gmail.com>
2023-07-20 13:18:47 +0900

    「認証関連の部品」追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {StatusBar} from 'expo-status-bar';
-import {RootStackNav} from 'navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { UserContextProvider } from './contexts/UserContext';
+import { UnauthedStackNav } from './navigation';
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const App = () => {
   return (
     <GestureHandlerRootView style={StyleSheet.absoluteFill}>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <RootStackNav />
-      </NavigationContainer>
+      <UserContextProvider>
+        <NavigationContainer>
+          <UnauthedStackNav />
+        </NavigationContainer>
+      </UserContextProvider>
     </GestureHandlerRootView>
   );
 };
