93ee8db849674f31c79e755141fdffe86a08a4fc
yonejima <catchandrelease9@gmail.com>
2023-07-20 17:42:56 +0900

    「Modalスクリーン」完了

--- src/navigation/AuthedStackNav.tsx
+++ src/navigation/AuthedStackNav.tsx
@@ -1,12 +1,42 @@
+import {Ionicons} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {MainTabNav} from 'navigation/MainTabNav';
-import React from 'react';
+import React, {useCallback, useContext} from 'react';
+import {Button, ThemeContext} from 'react-native-elements';
+import {TodoForm} from 'screens';
+
+const CloseButton: React.FC = () => {
+  const {theme} = useContext(ThemeContext);
+  const navigation = useNavigation();
+  const onClose = useCallback(() => navigation.goBack(), [navigation]);
+
+  return (
+    <Button
+      type="clear"
+      icon={<Ionicons name="md-close" color={theme.colors?.primary} size={30} />}
+      onPress={onClose}
+    />
+  );
+};
 
 const nav = createStackNavigator();
+
 export const AuthedStackNav: React.FC = () => {
   return (
-    <nav.Navigator screenOptions={{headerShown: false}}>
+    <nav.Navigator screenOptions={{headerShown: false}} initialRouteName="Main">
       <nav.Screen name="Main" component={MainTabNav} />
+      <nav.Screen
+        name="TodoForm"
+        component={TodoForm}
+        options={{
+          headerShown: true,
+          presentation: 'modal',
+          headerLeft: () => undefined,
+          headerRight: () => <CloseButton />,
+          headerTransparent: true,
+        }}
+      />
     </nav.Navigator>
   );
 };
